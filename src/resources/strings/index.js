9d229839d0f4b8259849922b3f82ac17e81ada07
Simplymichael <simplymichaelorji@gmail.com>
2020-05-20 01:24:34 +0100

    feat(resources/strings): dynamically detect browser language
    Try to detect browser's language and use it; fallback to english if undetectable or language file
    not found

--- src/resources/strings/index.js
+++ src/resources/strings/index.js
@@ -1,6 +1,20 @@
+/**
+ * Attempt to detect browser's language.
+ * If not set, or if language file not found, use default configured language.
+ * If default configured language not found, default to English.
+ */
+
 import config from '../../config'
 
+const userLanguage = navigator.language || navigator.userLanguage;
 const { language } = config;
-const activeLanguage = require('./' + language)['default'];
+const availableLanguage = userLanguage || language;
+let activeLanguage;
+
+try {
+  activeLanguage = require('./' + availableLanguage)['default'];
+} catch(err) {
+  activeLanguage = require('./en')['default'];
+}
 
 export default activeLanguage;
