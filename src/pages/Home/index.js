91fe5ebb2ec037b3c76f89b52b30017c58e01483
Simplymichael <simplymichaelorji@gmail.com>
2020-05-24 00:30:06 +0100

    feat(home page): implement products listing on Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import ProductsList from '../../components/Products/ProductsList';
 
-const Home = () => {
-  return (
-    <Row>
-      <Col md="3" role="sidebar">
-        Sidebar
-      </Col>
-      <Col md="9" role="main-content">
-        Home page
-      </Col>
-    </Row>
-  );
-};
+const Home = () => (
+  <Row>
+    <Col md="3" role="sidebar">
+      Sidebar
+    </Col>
+    <Col md="9" role="main-content">
+      <Row role="products-list-container">
+        <ProductsList weight="4" />
+      </Row>
+    </Col>
+  </Row>
+);
 
 export default Home;
