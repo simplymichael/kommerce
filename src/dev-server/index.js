a21d4e113b80f60211262e2c4fd59bd0d7281866
Simplymichael <simplymichaelorji@gmail.com>
2020-05-30 16:28:29 +0100

    feat(requests): support pagination and limiting of results
    Implement support for pagination and limiting of results on the dev server; Re-implement some route
    definitions from the src/dev-server/routes.js file into a middleware the src/dev-server/index.js
    file;

--- src/dev-server/index.js
+++ src/dev-server/index.js
@@ -22,6 +22,33 @@ server.use((req, res, next) => {
   next(); // Continue to JSON Server router
 });
 
+// embed product-reviews in every request for products
+server.use((req, res, next) => {
+  if(req.path.indexOf('/products') === 0 && !req.query._embed) {
+    req.query._embed = 'product-reviews';
+  }
+
+  next();
+});
+
+// Replace query strings with json-server-specific equivalents
+server.use((req, res, next) => {
+  const replacements = {
+    page: '_page',
+    limit: '_limit',
+    embed: '_embed',
+  };
+
+  for(let [key, replacement] of Object.entries(replacements)) {
+    if(req.query[key]) {
+      req.query[replacement] = req.query[key];
+      delete req.query[key];
+    }
+  }
+
+  next();
+});
+
 
 server.use(jsonServer.rewriter(routes)); // Support custom routes
 server.use(router); // Use default router
