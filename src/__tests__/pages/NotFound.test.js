50185e0a5e82cc6e37835e500f25cca8e4053ca6
Simplymichael <simplymichaelorji@gmail.com>
2020-06-07 18:01:10 +0100

    refactor(not found page): update Not found page to include link back to home page

--- src/__tests__/pages/NotFound.test.js
+++ src/__tests__/pages/NotFound.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import NotFound from '../../pages/NotFound';
+import {
+  store,
+  bindComponentToStore,
+  wrapComponentInRouter
+} from '../test-utils';
+
+let Component;
+const ConnectedComponent = bindComponentToStore(store)(
+  wrapComponentInRouter(NotFound));
+
+beforeEach(() => {
+  Component = render(
+    <ConnectedComponent />
+  );
+});
+
+afterEach(cleanup);
 
 describe('404 error Page', () => {
-  it('renders text: 404: Page Not Found', () => {
-    const { getByText } = render(<NotFound />);
-    const text = getByText(/404: Page Not Found/i);
-    expect(text).toBeInTheDocument();
+  it('renders link back to home', () => {
+    const { getByRole } = Component;
+    const homeBackLink = getByRole('back-to-home-link');
+    expect(homeBackLink).toBeInTheDocument();
   });
 });
--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Col, Row } from 'react-bootstrap';
 
 const NotFound = () => {
   return (
     <Row>
-      <Col md="3" role="sidebar">
-        Sidebar
-      </Col>
+      <Col md="3" role="sidebar"></Col>
       <Col md="9" role="main-content">
-        404: Page Not Found
+        <p>
+          Oops!!!
+          It looks like we couldn&apos;t find the page you&apos;re looking for.
+        </p>
+        <p>
+          <Link to="/" role="back-to-home-link">Click here</Link>&nbsp;
+          to get back to the home page.
+        </p>
       </Col>
     </Row>
   );
